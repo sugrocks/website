f4e4bf99f49cc1d2663435da00f89b16de00ee5a
MKody <im@kdy.ch>
2017-01-03 07:35:26 +0000

    Bomb leaked

--- public/countdown/episodes.js
+++ public/countdown/episodes.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 // addEpisode('SXXEXX', 'TITLE', 'utc', YYYY, M, D, h, m, dur, leaked, unknownDate)
-addEpisode('S04E10', 'Steven’s Dream', 'utc', 2017, 1, 30, 0, 0, 11, 0, 0)
-addEpisode('S04E11', 'Adventures in Light Distortion', 'utc', 2017, 1, 30, 0, 15, 11, 0, 0)
-addEpisode('S04E12', 'Gem Heist', 'utc', 2017, 1, 31, 0, 0, 11, 0, 0)
-addEpisode('S04E13', 'The Zoo', 'utc', 2017, 2, 1, 0, 0, 11, 0, 0)
-addEpisode('S04E14', 'That Will Be All', 'utc', 2017, 2, 2, 0, 0, 11, 0, 0)
+addEpisode('S04E10', 'Steven’s Dream', 'utc', 2017, 1, 30, 0, 0, 11, 1, 0)
+addEpisode('S04E11', 'Adventures in Light Distortion', 'utc', 2017, 1, 30, 0, 15, 11, 1, 0)
+addEpisode('S04E12', 'Gem Heist', 'utc', 2017, 1, 31, 0, 0, 11, 1, 0)
+addEpisode('S04E13', 'The Zoo', 'utc', 2017, 2, 1, 0, 0, 11, 1, 0)
+addEpisode('S04E14', 'That Will Be All', 'utc', 2017, 2, 2, 0, 0, 11, 1, 0)
