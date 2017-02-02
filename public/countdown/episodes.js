18f2f0ea037ea1464293583364d14d9e766c00f4
MKody <im@kdy.ch>
2017-02-02 21:00:20 +0000

    oh

--- public/countdown/episodes.js
+++ public/countdown/episodes.js
@@ -3,4 +3,4 @@
 addEpisode('S04E14', 'That Will Be All', 'utc', 2017, 2, 3, 0, 0, 11, 1, 0)
 addEpisode('S04E15', 'The New Crytal Gems', 'utc', 2017, 2, 11, 0, 0, 11, 0, 0)
 addEpisode('S04E16', 'Storm In The Room', 'utc', 2017, 2, 17, 23, 45, 11, 0, 0)
-addEpisode('S04E17', 'Rocknaldo', 'utc', 2017, 2, 25, 00, 00, 11, 0, 0)
+addEpisode('S04E17', 'Rocknaldo', 'utc', 2017, 2, 25, 0, 0, 11, 0, 0)
