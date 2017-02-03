28046877d247cc2b0fc72869ee7212d8aeb1e458
MKody <im@kdy.ch>
2017-02-03 14:08:18 +0000

    Typo. Also bumping to official episodes numbers

--- public/countdown/episodes.js
+++ public/countdown/episodes.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-undef */
 // addEpisode('SXXEXX', 'TITLE', 'utc', YYYY, M, D, h, m, dur, leaked, unknownDate)
-addEpisode('S04E14', 'That Will Be All', 'utc', 2017, 2, 3, 0, 0, 11, 1, 0)
-addEpisode('S04E15', 'The New Crytal Gems', 'utc', 2017, 2, 11, 0, 0, 11, 0, 0)
-addEpisode('S04E16', 'Storm In The Room', 'utc', 2017, 2, 17, 23, 45, 11, 0, 0)
-addEpisode('S04E17', 'Rocknaldo', 'utc', 2017, 2, 25, 0, 0, 11, 0, 0)
+addEpisode('S04E16', 'The New Crystal Gems', 'utc', 2017, 2, 11, 0, 0, 11, 0, 0)
+addEpisode('S04E17', 'Storm In The Room', 'utc', 2017, 2, 17, 23, 45, 11, 0, 0)
+addEpisode('S04E18', 'Rocknaldo', 'utc', 2017, 2, 25, 0, 0, 11, 0, 0)
