5c05fabeb613fda09d2d2dfa89e3c5f648802856
MKody <im@kdy.ch>
2017-02-04 22:20:51 +0000

    Tiger Philanthropist added to countdown

--- public/countdown/episodes.js
+++ public/countdown/episodes.js
@@ -3,3 +3,4 @@
 addEpisode('S04E16', 'The New Crystal Gems', 'utc', 2017, 2, 11, 0, 0, 11, 0, 0)
 addEpisode('S04E17', 'Storm In The Room', 'utc', 2017, 2, 17, 23, 45, 11, 0, 0)
 addEpisode('S04E18', 'Rocknaldo', 'utc', 2017, 2, 25, 0, 0, 11, 0, 0)
+addEpisode('S04E19', 'Tiger Philanthropist', 'utc', 2017, 3, 04, 0, 0, 11, 0, 0)
