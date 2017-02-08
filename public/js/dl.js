43fca0b613737bd35d6d084dcedf310ab3be5d99
MKody <im@kdy.ch>
2017-02-08 09:13:45 +0000

    Add legal 'warning' on DL page; small things

---
+++ public/js/dl.js
@@ -0,0 +1,6 @@
+/* === Toggle to show downloads === */
+function youAreAPirate () { // eslint-disable-line
+  var dlList = document.querySelector('#ImOKWithThat')
+  dlList.style.display = 'block'
+}
+
