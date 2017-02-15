48cc815bdc1135e35a93b7013b0dc87c72f8407e
MKody <im@kdy.ch>
2017-02-15 07:48:13 +0000

    Use a checkbox to show/hide DLs

--- public/js/dl.js
+++ public/js/dl.js
@@ -1,6 +1,23 @@
 /* === Toggle to show downloads === */
-function youAreAPirate () { // eslint-disable-line
+function youAreAPirate (checked) { // eslint-disable-line
   var dlList = document.querySelector('#ImOKWithThat')
-  dlList.style.display = 'block'
+  var date = new Date()
+  date.setTime(date.getTime() + (365 * 24 * 60 * 60 * 1000))
+
+  if (checked) {
+    dlList.style.display = 'block'
+    document.cookie = 'WhoAmI=You are a pirate!; expires=' + date.toGMTString() + '; path=/'
+  } else {
+    dlList.style.display = 'none'
+    document.cookie = 'WhoAmI=You are a great person!; expires=' + date.toGMTString() + '; path=/'
+  }
 }
 
+function checkIfImAPirate () {
+  if (readCookie('WhoAmI') === 'You are a pirate!') {
+    youAreAPirate(true)
+    document.querySelector('#yarr').checked = true
+  }
+}
+
+checkIfImAPirate()
