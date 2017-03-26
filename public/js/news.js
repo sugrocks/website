5e91b2461de438a6be00cbbe42cddf9156a5cd49
MKody <im@kdy.ch>
2017-03-26 15:45:43 +0000

    Re-organize sidebar; Fix table for comics; prepare ad spot

--- public/js/news.js
+++ public/js/news.js
@@ -19,5 +19,33 @@ function checkNews () {
   xhttp.send()
 }
 
+/* === Display the plug (ad) === */
+function plugBanner (ready) {
+  if (ready === 'no') return false
+
+  var linkPlug = document.querySelector('.plug a')
+  var deskPlug = document.querySelector('.plug .is-hidden-mobile')
+  var mobilePlug = document.querySelector('.plug .is-hidden-tablet')
+
+  var xhttp = new XMLHttpRequest()
+
+  xhttp.onreadystatechange = function () {
+    if (xhttp.readyState === 4 && xhttp.status === 200) {
+      if (xhttp.responseText.trim() !== '') {
+        var plugs = JSON.parse(xhttp.responseText)
+        var random = plugs[Math.floor(Math.random() * plugs.length)]
+
+        linkPlug.href = random['url']
+        deskPlug.src = random['desktop']
+        mobilePlug.src = random['mobile']
+      }
+    }
+  }
+
+  xhttp.open('GET', 'https://sug.rocks/plugs.json?' + Math.floor((Math.random() * 99999) + 1), true)
+  xhttp.send()
+}
+
+plugBanner('no')
 checkNews()
 setInterval(checkNews, 120000)
